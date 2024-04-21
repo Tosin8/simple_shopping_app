cb782c6575202ddfe3ffc7c4b7df906c830dba75
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-21 16:20:54 +0100

    updating with function for registration in firebase

--- lib/auth/controller/controller.dart
+++ lib/auth/controller/controller.dart
@@ -1,8 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
+import '../../shop/home/home.dart';
+import '../login/login.dart';
+
 class AuthController extends GetxController{
 
-  final AuthController instance = Get.find(); 
+  static AuthController instance = Get.find(); 
   late Rx<User?> _user; 
+
+  FirebaseAuth auth = FirebaseAuth.instance; // to be able to access alot of property from firebase. 
+
+  @override
+  void onReady(){
+    super.onReady(); 
+    _user = Rx<User?>(auth.currentUser); // to call the user model anywhere on the app
+    _user.bindStream(auth.userChanges()); // to listen to user changes
+    ever(_user, _initialScreen); 
+  }
+
+// taking the user to the homepage, condition if the user is login or not. 
+
+
+  _initialScreen(User? user){
+    if(user==null){
+      print('login page');
+       Get.offAll(()=>const LoginScreen());}
+       else{
+        Get.offAll( ()=>const HomeScreen());
+       }
+  }
+// create function for registration 
+void register(String email, password,) {
+auth.createUserWithEmailAndPassword(email: email, password: password); 
+  }
+  
+
 }
