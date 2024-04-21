c18eb5b9789ff8645dfc4faa845b254a8330c0ae
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-21 15:28:59 +0100

    Update controller.dart

--- lib/auth/controller/controller.dart
+++ lib/auth/controller/controller.dart
@@ -1,3 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
-class AuthController extends GetxController{}
+class AuthController extends GetxController{
+
+  final AuthController instance = Get.find(); 
+  late Rx<User?> _user; 
+}
