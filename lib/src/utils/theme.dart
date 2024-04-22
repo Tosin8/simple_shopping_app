02ca5c12c2ce48499b1197bc71242a147a6493b1
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-22 19:40:45 +0100

    Updating text theme and theme data

--- lib/src/utils/theme.dart
+++ lib/src/utils/theme.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+import 'text_theme.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light, 
-    textTheme: TextTheme(
-      displayMedium: GoogleFonts.montserrat(
-        color: Colors.black87, 
-      ), 
-      titleSmall: GoogleFonts.poppins(color: Colors.black54, 
-      fontSize: 24), 
-    )
-    );
+    textTheme:  ATextTheme.lightTextTheme , 
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+    
+    )), 
+  ); 
 
+  
     // for dark mode.  
   static ThemeData darkTheme = ThemeData(brightness: Brightness.dark, 
-   textTheme: TextTheme(
-      displayMedium: GoogleFonts.montserrat(
-        color: Colors.white70, 
-      ), 
-      titleSmall: GoogleFonts.poppins(color: Colors.white60, 
-      fontSize: 24), 
-    )
+   textTheme: ATextTheme.darkTextTheme, 
   );
 }
