6a227fe3523c13679b9078a9ba873a9b9cadb916
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-22 20:18:57 +0100

    Create sizes

--- lib/src/constants/colors.dart
+++ lib/src/constants/colors.dart
@@ -2,3 +2,10 @@
 import 'package:flutter/material.dart'; 
 
 const tPrimaryColor = Color(0xffffe400); 
+const tSecondaryColor = Color(0xff272727); 
+const tAccentColor = Color(0xff001bff); 
+
+
+const tWhiteColor = Colors.white; 
+const tDarkColor = Color(0xff000000); 
+const tCardBgColor = Color(0xfff7f6f1); 
