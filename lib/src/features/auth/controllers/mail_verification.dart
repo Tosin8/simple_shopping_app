6aaa4727814e111dd34c4637d8b71ce8fcc5573f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-24 21:56:15 +0100

    Update mail_verification.dart

--- lib/src/features/auth/controllers/mail_verification.dart
+++ lib/src/features/auth/controllers/mail_verification.dart
@@ -1,5 +1,15 @@
 import 'package:get/get.dart';
 
 class MailVerificationController extends GetxController {
-  
+  @override 
+  void onInit() {
+    super.onInit(); 
+    sendVerificationEmail(); 
+  }
+
+  void sendVerificationEmail(){} 
+
+  void setTimerForAutoRedirect(){} 
+
+  void manuallyCheckEmailVerificationStatus(){}
 }
